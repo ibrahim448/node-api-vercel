432940f1c2f026e2a1c42ccc87c77fbb345c09b8
Ibrahim <mdibrahim.bd320@gmail.com>
2023-09-10 15:38:31 +0600

    add json

--- index.js
+++ index.js
@@ -5,14 +5,40 @@ const cors = require("cors");
 
 app.use = (cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const news = require("./data/news.json");
+const categories = require("./data/Category.json");
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello Dragon")
 })
-app.get('/news', (req, res) => {
-  res.send('News!')
-})
+
 app.get('/categories', (req, res) => {
-  res.send('All Categories')
+  res.send(categories)
+});
+
+app.get("/news", (req,res)=>{
+    res.send(news);
+});
+
+app.get("/news/:id",(req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n=> n._id == id);
+    res.send(selectedNews); 
+});
+
+app.get("/categories/:id", (req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const categoriesNews = news.filter(n=> n.category_id == id);
+        res.send(categoriesNews)
+    }
+    
 })
 
 app.listen(port, () => {
